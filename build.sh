fdf3a253cc0b4704c3554314a8157db980c6cc56
Beth Yates <byates@ebi.ac.uk>
2018-12-11 15:09:27 +0000

    testing build

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 yum -y update && yum clean all;
+yum -y install wget
+yum -y install tar
+yum -y install gcc
+yum -y install zlib-devel
+yum -y install sudo
+yum -y install git
+
+yum -y install perl-CPAN
+curl -L http://cpanmin.us | perl - App::cpanminus

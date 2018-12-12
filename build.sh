119363908ff06adabdb64e095769751a861659cf
Beth Yates <byates@ebi.ac.uk>
2018-12-12 09:26:38 +0000

    added DBD modules

--- build.sh
+++ build.sh
@@ -8,5 +8,15 @@ yum -y install zlib-devel
 yum -y install sudo
 yum -y install git
 
+service iptables stop;
+chkconfig iptables off;
+rm -rf /etc/rc.d/init.d/iptables
+
 yum -y install perl-CPAN
 curl -L http://cpanmin.us | perl - App::cpanminus
+
+wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+rpm -ivh epel-release-6-8.noarch.rpm
+
+yum -y install perl-DBD-mysql perl-DBI
+yum -y install perl-DBD-Pg
